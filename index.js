b69665b70c3f495c57907ababa838f3d2498e62a
QarthO <QarthO@gmail.com>
2023-06-04 20:31:35 -0400

    reverted packagejson

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var twichatRouter = require('./routes/twichat/route.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'jade')
 app.use('/', twichatRouter);
 
 // catch 404 and forward to error handler
